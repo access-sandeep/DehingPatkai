7afb0aeeb5325e1ce0b018def1e3bd2e7e13c234
Mukherji, Sandeep <sandeep.talk@gmail.com>
2023-07-05 21:56:41 +0530

    some changes

--- dehing_patkai/src/App.tsx
+++ dehing_patkai/src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div><img src={logo} className="App-logo" alt="logo" /></div>
+        <div>
+          <div id="glow-ingress-block">
+              <span className="nav-line-1 nav-progressive-content" id="glow-ingress-line1">
+                  Deliver to Sandeep
+              </span>
+              <span className="nav-line-2 nav-progressive-content" id="glow-ingress-line2">
+                  Chennai 600130&zwnj;
+              </span>
+          </div>
+        </div>
+        Header placeholder
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        Main area
+      </main>
+      <footer>
+        Footer Content
+      </footer>
     </div>
   );
 }
